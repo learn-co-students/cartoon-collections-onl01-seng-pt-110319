f77e2efba52e7c4466be7d6895f18d03d5288270
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-24 00:05:34 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,40 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+
+
+
+def roll_call_dwarves(names)
+  i = 0
+  while i < names.length
+    puts "#{i + 1}. #{names[i]}"
+  i += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(array)
+    array.map  do |element|  
+    element.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(calls)
+     calls.each do |calls|
+      if calls.length > 4
+      return true
+        end
+      end
+      return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |cheese|
+    cheese_types.include?(cheese)
+    end
+
 end
+
+
+
+
